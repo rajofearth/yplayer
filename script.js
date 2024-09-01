2a6d548d87b98c5c0d51dbbc40c06a41183c0847
Yashraj Maher <rajofearth@proton.me>
2024-09-01 11:14:12 +0530

    QOL Improvements P1

--- script.js
+++ script.js
@@ -319,7 +319,7 @@ play() {
                         <p class="text-sm text-[#00ff00]/70">${track.artist}</p>
                     </div>
                 </div>
-                <button class="play-track-btn bg-[#00ff00] hover:bg-[#00ff00]/80 text-[#001122] rounded-full p-2" data-index="${index}">
+                <button class="play-track-btn bg-[#00ff00] hover:bg-[#00ff00]/80 text-[#001122] rounded-full p-2" id="current-playing-s-play-btn" data-index="${index}">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z" />
                     </svg>
@@ -339,10 +339,17 @@ play() {
                 const index = parseInt(e.currentTarget.dataset.index);
                 this.currentTrackIndex = index;
                 this.play();
+                updatePlayPauseButtonListed();
             });
         });
     }
-
+    updatePlayPauseButtonListed() {
+        const button = document.getElementById('current-playing-s-play-btn');
+        button.innerHTML = this.isPlaying
+            ? '<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 9v6m4-6v6m7-3a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>'
+            : '<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z" /><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>';
+        button.className = 'bg-[#00ff00] hover:bg-[#00ff00]/80 text-[#001122] rounded-full p-2 sm:p-3';
+    }
     updatePlayPauseButton() {
         const button = document.getElementById('play-pause-btn');
         button.innerHTML = this.isPlaying
